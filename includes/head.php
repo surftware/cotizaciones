e9cb3cf64ed19a0112209ba5b3386aa0387a1cdc
rafael <rafael.madrigal.ciencias.unam.mx>
2021-05-03 16:18:29 -0500

    includes_header_footer

---
+++ includes/head.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+
+	<!-- Basic -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<title>Cotizacion</title>
+
+	<meta name="keywords" content="HTML5 Template" />
+	<meta name="description" content="EZY - Responsive HTML5 Template">
+	<meta name="author" content="okler.net">
+
+	<!-- Mobile Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+
+	<!--favicon -->
+	<link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
+	<link rel="manifest" href="assets/favicon/site.webmanifest">
+	<link rel="mask-icon" href="assets/favicon/safari-pinned-tab.svg" color="#212529">
+	<meta name="msapplication-TileColor" content="#212529">
+	<meta name="theme-color" content="#ffffff">
+
+	<!-- Web Fonts  -->
+	<link
+		href="https://fonts.googleapis.com/css?family=Montserrat:100,300,400,500,600,700,900%7COpen+Sans:300,400,600,700,800"
+		rel="stylesheet" type="text/css">
+
+	<!-- Vendor CSS -->
+	<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="vendor/font-awesome/css/fontawesome-all.min.css">
+	<link rel="stylesheet" href="vendor/animate/animate.min.css">
+	<link rel="stylesheet" href="vendor/linear-icons/css/linear-icons.min.css">
+	<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
+	<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
+	<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
+
+	<!-- Theme CSS -->
+	<link rel="stylesheet" href="css/theme.css">
+	<link rel="stylesheet" href="css/theme-elements.css">
+
+	<!-- Skin CSS -->
+	<link rel="stylesheet" href="css/skins/skin-gym.css">
+	<!--<link rel="stylesheet" href="css/skins/default.css">-->
+
+	<!-- Theme Custom CSS -->
+	<link rel="stylesheet" href="css/custom.css">
+	<link rel="stylesheet" href="css/design.css">
+	<link rel="stylesheet" href="css/footer.css">
+
+
+	<!-- Head Libs -->
+	<script src="vendor/modernizr/modernizr.min.js"></script>
+
+</head>
---
+++ includes/header.php
@@ -0,0 +1,97 @@
+<body>
+
+    <div class="body">
+        <header id="header" class="header-effect-shrink"
+            data-plugin-options="{'stickyEnabled': true, 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': true, 'stickyStartAt': 120}">
+            <div class="header-body">
+                <div class="header-top">
+                    <div class="header-top-container container">
+                        <div class="header-row">
+                            <div class="header-column justify-content-start">
+                                <span class="d-none d-sm-flex align-items-center">
+                                    <i class="fas fa-map-marker-alt mr-1"></i>
+                                    Norte 84 A 6515 San Pedro el Chico, CDMX
+                                </span>
+                                <span class="d-none d-sm-flex align-items-center ml-4">
+                                    <i class="fas fa-phone mr-1"></i>
+                                    <a href="tel:+525511895621">5511894621</a>
+                                </span>
+                            </div>
+                            <div class="header-column justify-content-end">
+                                <!--<ul class="nav">
+									<li class="nav-item">
+										<a class="nav-link" href="contact-us-1.html">Contact Us</a>
+									</li>
+									<li class="nav-item">
+										<a href="#" class="nav-link dropdown-menu-toggle py-2" id="dropdownLanguage"
+											data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+											English <i class="fas fa-angle-down fa-sm"></i>
+										</a>
+										<ul class="dropdown-menu dropdown-menu-right"
+											aria-labelledby="dropdownLanguage">
+											<li><a href="#" class="no-skin"><img src="img/blank.gif"
+														class="flag flag-us" alt="English" /> English</a></li>
+											<li><a href="#" class="no-skin"><img src="img/blank.gif"
+														class="flag flag-es" alt="Español" /> Español</a></li>
+											<li><a href="#" class="no-skin"><img src="img/blank.gif"
+														class="flag flag-fr" alt="Française" /> Française</a></li>
+										</ul>
+									</li>
+								</ul>-->
+                                <ul
+                                    class="header-top-social-icons social-icons social-icons-transparent d-none d-md-block">
+                                    <li class="social-icons-facebook">
+                                        <a href="https://www.facebook.com/AnDmanDo/" target="_blank" title="Facebook"><i
+                                                class="fab fa-facebook-f"></i></a>
+                                        <!--</li>
+									<li class="social-icons-twitter">
+										<a href="http://www.twitter.com/" target="_blank" title="Twitter"><i
+												class="fab fa-twitter"></i></a>
+									</li>
+									<li class="social-icons-instagram">
+										<a href="http://www.instagram.com/" target="_blank" title="Instragram"><i
+												class="fab fa-instagram"></i></a>
+									</li>-->
+                                </ul>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <div role="main" class="main">
+
+            <section class="page-header">
+                <div class="container">
+
+                    <div class="row">
+                        <div class="col-md-2">
+                            <div class="header-column justify-content-start">
+                                <div class="header-logo">
+                                    <a href="index.html">
+                                        <img alt="Surftware" width="127" height="49"
+                                            src="assets/img/creacion-paginas-web.png">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="header-column justify-content-start">
+                                <h1>COTIZACIÓN</h1>
+                                <p class="lead">Equipo de Cómputo Escritorio</p>
+                            </div>
+                        </div>
+
+                        <div class="col-md-2">
+                            <div class="header-column justify-content-start">
+                                <div class="header-logo">
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>    
+            </section>
